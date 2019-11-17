6487c2d0dc0d53759fef6b50e7bb6cd2eb7ae7cf
Minira Samadova <samadova.minira@gmail.com>
2019-11-17 23:13:11 +0900

    redirect from http to https

--- server/server.js
+++ server/server.js
@@ -6,13 +6,16 @@ app.use(express.static(path.join(__dirname, "..", "build")));
 
 const PORT = process.env.PORT || 4000;
 
+app.use(function(request, response){
+  if(!request.secure){
+    response.redirect("https://" + request.headers.host + request.url);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Environment: ${process.env.NODE_ENV || "development"}`);
   console.log(`App listening on port ${PORT}!`);
 });
 
-app.get("*", function(request, response){
-  response.redirect("https://" + request.headers.host + request.url);
-});
 
 
