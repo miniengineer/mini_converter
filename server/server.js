21a2f46ad59c4b3e2f415bdaa444a25c153fd423
Minira Samadova <samadova.minira@gmail.com>
2019-11-17 23:49:07 +0900

    redirect from http to https

--- server/server.js
+++ server/server.js
@@ -4,12 +4,11 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "..", "build")));
 
-if(process.env.NODE_ENV === 'production') {
-  app.use((req, res, next) => {
-    if (req.header('x-forwarded-proto') !== 'https') res.redirect(`https://${req.header('host')}${req.url}`);
-    else next();
-  });
-};
+app.use((req, res, next) => {
+  if (req.header('x-forwarded-proto') !== 'https') res.redirect(`https://${req.header('host')}${req.url}`);
+  else next();
+});
+
 
 const PORT = process.env.PORT || 4000;
 
