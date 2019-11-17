19db0204cf9353ee2cd0ebd0d58fb77033745905
Minira Samadova <samadova.minira@gmail.com>
2019-11-17 23:29:18 +0900

    redirect from http to https

--- server/server.js
+++ server/server.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const sslRedirect = require(‘heroku-ssl-redirect’);
 
 app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(sslRedirect());
 
 const PORT = process.env.PORT || 4000;
-
-app.use(function(request, response){
-  if(!request.secure){
-    response.redirect("https://" + request.headers.host + request.url);
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Environment: ${process.env.NODE_ENV || "development"}`);
